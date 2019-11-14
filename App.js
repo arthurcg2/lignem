b0f2d99505adedb01d308124f7632a445355bf89
Arthur Guerra <conta.acg2002@gmail.com>
2019-11-13 23:18:32 -0300

    Last setup commit

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { StatusBar, View, Text } from 'react-native';
 
 export default function App() {
 	return (
-		<View>
-			<Text>Lignem???</Text>
-		</View>
+		<>
+			<StatusBar barStyle="dark-content" backgroundColor="white" />
+			<View
+				style={{
+					flex: 1,
+					backgroundColor: 'white',
+					justifyContent: 'center',
+					alignItems: 'center',
+				}}
+			>
+				<Text style={{ color: '#7159C1', fontSize: 32, fontWeight: 'bold' }}>
+					Lignem
+				</Text>
+			</View>
+		</>
 	);
 }
