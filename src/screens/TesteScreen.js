74f515629e761de297743bedabeae09a955fbd1b
Arthur Guerra <conta.acg2002@gmail.com>
2019-12-19 23:38:12 -0300

    adicionando json de conteudo

--- src/screens/TesteScreen.js
+++ src/screens/TesteScreen.js
@@ -1,68 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { ScrollView } from 'react-native';
 import Markdown from 'react-native-markdown-package';
+import { teste } from '../../contents/contents';
 
-const Teste = () => {
-	const [mdText, setMdText] = useState('Não definido');
-
-	useEffect(() => {
-		setMdText(async () => {
-			const text = await fetch('../../docs/setup.md');
-			console.log(text);
-			return text.text;
-		});
-	});
-
+const TesteScreen = () => {
 	return (
 		<ScrollView>
-			<Markdown styles={MarkdownStyles} onLink={url => Linking.openURL(url)}>
-				{mdText}
-			</Markdown>
+			<Markdown>{teste}</Markdown>
 		</ScrollView>
 	);
 };
 
-Teste.navigationOptions = {
+TesteScreen.navigationOptions = {
 	title: 'Teste',
 };
 
-const MarkdownStyles = {
-	heading1: {
-		color: 'red',
-	},
-	heading2: {
-		color: 'green',
-		textAlign: 'right',
-	},
-	strong: {
-		color: 'blue',
-	},
-	em: {
-		color: 'cyan',
-	},
-	text: {
-		color: 'black',
-	},
-	blockQuoteText: {
-		color: 'grey',
-	},
-	blockQuoteSection: {
-		flexDirection: 'row',
-	},
-	blockQuoteSectionBar: {
-		width: 3,
-		height: null,
-		backgroundColor: '#DDDDDD',
-		marginRight: 15,
-	},
-	codeBlock: {
-		fontFamily: 'Courier',
-		fontWeight: '500',
-		backgroundColor: '#DDDDDD',
-	},
-	tableHeader: {
-		backgroundColor: 'grey',
-	},
-};
-
-export default Teste;
+export default TesteScreen;
