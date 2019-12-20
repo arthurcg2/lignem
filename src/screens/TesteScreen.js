36ad46ed6512f3465dc412b7edb644a4b0907345
Arthur Guerra <conta.acg2002@gmail.com>
2019-12-20 01:38:21 -0300

    mudando renderer de markdown e adicionando caminho relativo

--- src/screens/TesteScreen.js
+++ src/screens/TesteScreen.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import { ScrollView, Linking } from 'react-native';
-import Markdown from 'react-native-markdown-package';
+import { ScrollView, Image } from 'react-native';
+import Markdown from 'react-native-simple-markdown';
 import { teste } from '../../contents/contents';
+import images from '../../contents/images';
+
+console.log(images);
 
 const TesteScreen = () => {
 	return (
 		<ScrollView>
 			<Markdown
-				onLink={url => {
-					Linking.openURL(url);
+				rules={{
+					image: {
+						react: (node, output, state) => {
+							return (
+								<Image
+									key={state.key}
+									source={images[node.target.split('.')[0]]}
+								/>
+							);
+						},
+					},
 				}}
 			>
 				{teste}
