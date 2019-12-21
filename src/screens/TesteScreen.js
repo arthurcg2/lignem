70095ddcd768a2445dd791699c2bfa58ce59fd3f
Arthur Guerra <conta.acg2002@gmail.com>
2019-12-20 22:49:19 -0300

    markdown styles

--- src/screens/TesteScreen.js
+++ src/screens/TesteScreen.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { ScrollView, Image } from 'react-native';
 import Markdown from 'react-native-simple-markdown';
+
 import { teste } from '../../contents/contents';
 import images from '../../contents/images';
+import { markdownStyles } from './styles';
 
 const TesteScreen = () => {
 	return (
 		<ScrollView>
 			<Markdown
+				styles={markdownStyles}
 				rules={{
 					image: {
 						react: (node, output, state) => {
 							return (
 								<Image
 									key={state.key}
+									style={markdownStyles.image}
 									source={images[node.target.split('.')[0]]}
 								/>
 							);
---
+++ src/screens/styles/index.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+
+export const markdownStyles = StyleSheet.create({
+	view: {
+		padding: 10,
+	},
+	image: {
+		width: 200,
+		height: 200,
+		resizeMode: 'contain',
+	},
+});
