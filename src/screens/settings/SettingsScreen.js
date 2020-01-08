7e14c9d885b8cbad149e423bd22a9990a8721598
Arthur Guerra <conta.acg2002@gmail.com>
2020-01-08 15:42:34 -0300

    Alterando a tela de configurações

--- src/screens/settings/SettingsScreen.js
+++ src/screens/settings/SettingsScreen.js
@@ -1,42 +1,102 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import { ListItem } from 'react-native-elements';
+import { View, ScrollView, Switch, Text, StyleSheet } from 'react-native';
+import { ListItem, Divider } from 'react-native-elements';
 import Logo from '../../components/Logo';
 
 const options = [
 	{
-		title: 'Modo de daltonismo',
-		sub: 'Configurações de daltonismo',
-		screenName: 'Daltonismo',
-		icon: 'colorize',
+		title: 'Tema escuro',
+		sub: 'Tons mais escuros para o Lignem',
+		icon: 'brightness-3',
+		rightElement: (
+			<Switch
+				value={false}
+				trackColor={{ true: '#937BE3' }}
+				thumbColor="#FFF"
+			/>
+		),
+		chevron: false,
 	},
 	{
-		title: 'Acessibilidade',
+		title: 'Sobre',
+		sub: 'Informações sobre o app',
+		icon: 'info',
+		navigate: 'Sobre',
+		chevron: true,
+	},
+];
+
+const accessibilityOptions = [
+	{
+		title: 'Modo de daltonismo',
+		sub: 'Configurações de daltonismo',
+		icon: 'colorize',
+		navigate: 'Daltonismo',
+		chevron: true,
+	},
+	{
+		title: 'Outros',
 		sub: 'Configurações gerais de acessiblidade',
-		screenName: 'Geral',
+		navigate: 'Outros',
 		icon: 'accessibility',
+		chevron: true,
 	},
 ];
 
 const Settings = ({ navigation }) => {
 	return (
-		<View style={styles.container}>
+		<ScrollView style={styles.container}>
+			<Text style={styles.title}>Geral</Text>
 			<View>
+				<Divider />
 				{options.map((item, i) => (
 					<ListItem
 						title={item.title}
 						subtitle={item.sub}
+						style={styles.listItem}
 						accessible
 						accessibilityLabel={item.sub}
-						onPress={() => navigation.navigate(item.screenName)}
 						leftIcon={{ name: item.icon }}
+						rightElement={item.rightElement ? item.rightElement : null}
+						onPress={() => {
+							if (item.navigate) {
+								navigation.navigate(item.navigate);
+							} else if (item.onPress) {
+								item.onPress();
+							}
+						}}
+						chevron={item.chevron}
 						bottomDivider
-						chevron
 						key={i}
 					/>
 				))}
 			</View>
-		</View>
+			<Text style={styles.title}>Acessibilidade</Text>
+			<View>
+				<Divider />
+				{accessibilityOptions.map((item, i) => (
+					<ListItem
+						title={item.title}
+						subtitle={item.sub}
+						style={styles.listItem}
+						accessible
+						accessibilityLabel={item.sub}
+						leftIcon={{ name: item.icon }}
+						rightElement={item.rightElement ? item.rightElement : null}
+						onPress={() => {
+							if (item.navigate) {
+								navigation.navigate(item.navigate);
+							} else if (item.onPress) {
+								item.onPress();
+							}
+						}}
+						chevron={item.chevron}
+						bottomDivider
+						key={i}
+					/>
+				))}
+			</View>
+		</ScrollView>
 	);
 };
 
@@ -48,6 +108,13 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: '#FFF',
+	},
+	title: {
+		fontSize: 30,
+		color: '#333',
+		marginLeft: 18,
+		marginTop: 10,
+		marginBottom: 20,
 	},
 });
 
