3f914a0e3d069a43e98b54418c174b0128e76177
GzinSocker <gabriel.cama@outlook.com>
2020-01-23 19:32:56 -0300

    Adicionada a estrutura basica do game

--- src/screens/interactives/InterativoTesteScreen.js
+++ src/screens/interactives/InterativoTesteScreen.js
@@ -1,12 +1,44 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
+import React, { useState } from 'react';
+import { View, Text, Dimensions } from 'react-native';
+import Draggable from 'react-native-draggable';
 
 import Logo from '../../components/Logo';
 
 const InterativoMain = () => {
+	const drSz = Dimensions.get('window').width - 100;
+	const stY = 50;
+	const stX = -40;
+	const [color, setColor] = useState('#cfbb80');
+	const changeableDist = 47;
+
+	var inX = 0;
+
+	function prog(afX) {
+		setTimeout(() => setColor('#cfbb80'), 250);
+		if (afX - inX > changeableDist) {
+			setColor('green');
+		} else if (afX - inX < changeableDist * -1) {
+			setColor('red');
+		}
+	}
+
 	return (
-		<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-			<Text>Tela de interativos</Text>
+		<View style={{ position: 'relative' }}>
+			<Draggable
+				renderSize={drSz}
+				renderColor={color}
+				x={Dimensions.get('window').width / 2 - drSz / 2}
+				y={stY}
+				minY={stY}
+				maxY={drSz + stY}
+				renderText=""
+				shouldReverse={true}
+				minX={stX}
+				maxX={Dimensions.get('window').width - stX}
+				onPressIn={evt => (inX = evt.nativeEvent.pageX)}
+				onDragRelease={evt => prog(evt.nativeEvent.pageX)}
+			/>
 		</View>
 	);
 };
