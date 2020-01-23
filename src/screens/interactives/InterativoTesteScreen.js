50c7b2c95b3cf8c653e4f91283ae4967de6e06c6
GzinSocker <gabriel.cama@outlook.com>
2020-01-23 19:50:09 -0300

    Adicionada uma pontuação(temporária obviamente)

--- src/screens/interactives/InterativoTesteScreen.js
+++ src/screens/interactives/InterativoTesteScreen.js
@@ -10,6 +10,7 @@ const InterativoMain = () => {
 	const stY = 50;
 	const stX = -40;
 	const [color, setColor] = useState('#cfbb80');
+	const [score, setScore] = useState(0);
 	const changeableDist = 47;
 
 	var inX = 0;
@@ -17,14 +18,16 @@ const InterativoMain = () => {
 	function prog(afX) {
 		setTimeout(() => setColor('#cfbb80'), 250);
 		if (afX - inX > changeableDist) {
+			setScore(score + 100);
 			setColor('green');
 		} else if (afX - inX < changeableDist * -1) {
+			score > 0 ? setScore(score - 100) : null;
 			setColor('red');
 		}
 	}
 
 	return (
-		<View style={{ position: 'relative' }}>
+		<View style={{ flex: 1, justifyContent: 'flex-end', alignItems: 'center' }}>
 			<Draggable
 				renderSize={drSz}
 				renderColor={color}
@@ -39,6 +42,8 @@ const InterativoMain = () => {
 				onPressIn={evt => (inX = evt.nativeEvent.pageX)}
 				onDragRelease={evt => prog(evt.nativeEvent.pageX)}
 			/>
+			<Text style={{ fontSize: 60 }}>{score}</Text>
+			<Text style={{ marginBottom: 40, fontSize: 30 }}>SCORE</Text>
 		</View>
 	);
 };
