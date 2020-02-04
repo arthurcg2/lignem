3a1568e4366474b52d632deb561c039f71344bec
Arthur Guerra <conta.acg2002@gmail.com>
2020-02-04 16:19:51 -0300

    refact: separando cards em um arquivo especifico

--- src/screens/content/HomeScreen.js
+++ src/screens/content/HomeScreen.js
@@ -4,32 +4,7 @@ import { Card } from 'react-native-elements';
 import Logo from '../../components/Logo';
 import { useThemeValue } from '../../states/ThemeState';
 
-const cards = [
-	{
-		id: 1,
-		title: 'Exemplo',
-		image: require('../../../assets/hidreletrica.jpg'),
-		description:
-			'Exercitation officia exercitation anim nostrud aliqua aliquip qui anim.',
-		buttonTitle: 'Ir para a página!',
-		targetPageSettings: {
-			contentJSONName: 'teste',
-			contentPageTitle: 'Teste!',
-		},
-	},
-	{
-		id: 2,
-		title: 'Energia Solar',
-		image: require('../../../assets/painel-solar.jpg'),
-		description:
-			'A energia solar: sua definição, funcionamento e suas características!.',
-		buttonTitle: 'Ir para a página!',
-		targetPageSettings: {
-			contentJSONName: 'solar',
-			contentPageTitle: 'Energia Solar',
-		},
-	},
-];
+import cards from './cards';
 
 const Home = ({ navigation }) => {
 	const [{ theme }] = useThemeValue();
---
+++ src/screens/content/cards.js
@@ -0,0 +1,26 @@
+export default [
+	{
+		id: 1,
+		title: 'Exemplo',
+		image: require('../../../assets/hidreletrica.jpg'),
+		description:
+			'Exercitation officia exercitation anim nostrud aliqua aliquip qui anim.',
+		buttonTitle: 'Ir para a página!',
+		targetPageSettings: {
+			contentJSONName: 'teste',
+			contentPageTitle: 'Teste!',
+		},
+	},
+	{
+		id: 2,
+		title: 'Energia Solar',
+		image: require('../../../assets/painel-solar.jpg'),
+		description:
+			'A energia solar: sua definição, funcionamento e suas características!.',
+		buttonTitle: 'Ir para a página!',
+		targetPageSettings: {
+			contentJSONName: 'solar',
+			contentPageTitle: 'Energia Solar',
+		},
+	},
+];
