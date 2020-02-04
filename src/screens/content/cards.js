dbdd4311a1e39922701b7c225e7863f5de079485
Arthur Guerra <conta.acg2002@gmail.com>
2020-02-04 17:25:17 -0300

    adicionado conteudo de maremotriz

--- src/screens/content/cards.js
+++ src/screens/content/cards.js
@@ -16,11 +16,23 @@ export default [
 		title: 'Energia Solar',
 		image: require('../../../assets/painel-solar.jpg'),
 		description:
-			'A energia solar: sua definição, funcionamento e suas características!.',
+			'A energia solar: sua definição, funcionamento e suas características!',
 		buttonTitle: 'Ir para a página!',
 		targetPageSettings: {
 			contentJSONName: 'solar',
 			contentPageTitle: 'Energia Solar',
+		},
+	},
+	{
+		id: 3,
+		title: 'Energia Maremotriz',
+		image: require('../../../assets/maremotriz.jpg'),
+		description:
+			'A energia maremotriz: sua definição, funcionamento e suas características!',
+		buttonTitle: 'Ir para a página!',
+		targetPageSettings: {
+			contentJSONName: 'maremotriz',
+			contentPageTitle: 'Energia Maremotriz',
 		},
 	},
 ];
