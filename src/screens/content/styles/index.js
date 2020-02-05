c8456be7ef428a65cedc8b464524bc6ba2084d1e
Arthur Guerra <conta.acg2002@gmail.com>
2020-02-04 22:44:22 -0300

    adicionando estilização ao markdown

--- src/screens/content/styles/index.js
+++ src/screens/content/styles/index.js
@@ -2,6 +2,9 @@ import { StyleSheet, Dimensions } from 'react-native';
 
 export const markdownStyles = StyleSheet.create({
 	view: {
+		borderWidth: 2,
+		borderColor: '#7159C1',
+		margin: 5,
 		padding: 10,
 	},
 	image: {
@@ -10,5 +13,37 @@ export const markdownStyles = StyleSheet.create({
 		left: Dimensions.get('window').width / 2 - 110,
 		marginVertical: 10,
 		resizeMode: 'contain',
+	},
+	paragraph: {
+		flexDirection: 'row',
+		flexWrap: 'wrap',
+		paddingVertical: 6,
+	},
+	list: {
+		paddingVertical: 6,
+		paddingLeft: 6,
+	},
+	heading1: {
+		fontSize: 32,
+		alignSelf: 'center',
+		fontWeight: '600',
+		paddingBottom: 10,
+		color: '#5947A9',
+	},
+	heading2: {
+		fontSize: 24,
+		color: '#333',
+	},
+	heading3: {
+		fontSize: 20,
+		color: '#444',
+	},
+	link: {
+		color: '#5947A9',
+		textDecorationLine: 'underline',
+	},
+	mailTo: {
+		color: '#5947A9',
+		textDecorationLine: 'underline',
 	},
 });
