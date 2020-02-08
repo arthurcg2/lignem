8c9561c6c0a546f2b959b922375b188bfdccfe43
Arthur Guerra <conta.acg2002@gmail.com>
2020-02-08 12:13:02 -0300

    adiciona card de hidrelétrica

--- src/screens/content/cards.js
+++ src/screens/content/cards.js
@@ -1,6 +1,18 @@
 export default [
 	{
 		id: 1,
+		title: 'Energia Hidrelétrica',
+		image: require('../../../assets/hidreletrica.jpg'),
+		description:
+			'As usinas hidrelétricas: suas características, detalhes e funcionamento!',
+		buttonTitle: 'Ir para a página!',
+		targetPageSettings: {
+			contentJSONName: 'hidreletrica',
+			contentPageTitle: 'Energia Hidrelétrica',
+		},
+	},
+	{
+		id: 2,
 		title: 'Energia Solar',
 		image: require('../../../assets/painel-solar.jpg'),
 		description:
@@ -12,7 +24,7 @@ export default [
 		},
 	},
 	{
-		id: 2,
+		id: 3,
 		title: 'Energia Maremotriz',
 		image: require('../../../assets/maremotriz.jpg'),
 		description:
@@ -24,7 +36,7 @@ export default [
 		},
 	},
 	{
-		id: 3,
+		id: 4,
 		title: 'Energia Eólica',
 		image: require('../../../assets/eolica-card.jpg'),
 		description:
