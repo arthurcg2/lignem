c89b039e2066f9ac2611da256fde5a530e442b74
Arthur Guerra <conta.acg2002@gmail.com>
2020-04-11 19:05:06 -0300

    Muda a cor da navigation bar dinamicamente

--- android/app/src/main/java/com/lignem/MainActivity.java
+++ android/app/src/main/java/com/lignem/MainActivity.java
@@ -27,25 +27,4 @@ public class MainActivity extends ReactActivity {
       }
     };
   }
-
-  @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        hideNavigationBar();
-    }
-
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-        if (hasFocus) {
-            hideNavigationBar();
-        }
-    }
-
-    private void hideNavigationBar() {
-        getWindow().getDecorView().setSystemUiVisibility(
-            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-
-    }
 }
